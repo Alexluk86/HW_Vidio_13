4dcfd5d96ffa8c07da7833c5c53ae633fb052867
Alex <Alex@LAPTOP-TBQG0DAV>
2021-01-31 16:46:01 +0400

    Добавьте файлы проекта.

---
+++ HW_Vidio_13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_Vidio_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.WriteLine("Введите Ваше число:");
+
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Ваше число чётное");
+            }
+            else
+            {
+                Console.WriteLine("Ваше число нечётное");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
